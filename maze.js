b7c76980159a94645e15bb00f2edebb88e39dea6
ratchet-Inc <deniroturboman@hotmail.com>
2016-10-11 09:35:41 -0700

    error checking

--- maze.js
+++ maze.js
@@ -1,1 +1,20 @@
 // JavaScript source code
+
+function addWallListneers(obj) {
+    console.log(obj.length);
+    for (var i = 0; i < obj.length - 1; i++) {
+        console.log(i);
+        obj[i].addEventListener("mouseover", function () {
+            this.className += " youlose";
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Page loaded.");
+    /*document.getElementById("boundary1").addEventListener("mouseover", function () {
+        this.className += " youlose";
+    });*/
+    var myLst = document.querySelectorAll(".boundary");
+    addWallListneers(myLst);
+});
