f823abdff69b41b4c4d60f46028202215b3a5a67
ratchet-Inc <deniroturboman@hotmail.com>
2016-10-11 00:36:28 -0700

    issues

--- lab4/lab4/maze.js
+++ lab4/lab4/maze.js
@@ -13,7 +13,16 @@ var addFailureClass = function (obj) {
     obj.className += " youlose";
 };
 
-var completioncheck = function () {
+var addFailListeners = function () {
+    var lst = document.querySelectorAll(".boundary");
+    console.log("length check: " + lst.length);
+    for (var i = 0; i < lst.length - 1; i++) {
+        lst[i].addEventListener("mouseover", function () { addFailureClass(lst[i]); });
+        console.log("div class name[" +i+"]: " + lst[i].className);
+    }
+}
+
+function completioncheck() {
     if(startedSignal == 1)
     {
         var lst = document.querySelectorAll(".youlose");
@@ -21,24 +30,26 @@ var completioncheck = function () {
         if (lst.length > 0) {
             console.log("challenge failed.");
         }
+        startedSignal = 0;
     }
-};
+}
 
-var restartMaze = function () {
+function restartMaze() {
     restartMaze = 1;
-    var lst = document.querySelectorAll(".boundry");
-    if(lst.length != 0)
+    var lst = document.querySelectorAll(".youlose");
+    console.log("restartMaze list lenght: " + lst.length);
+    if(lst.length > 0)
     {
-        for(var i = 0; i < (lst.length - 1); i++)
+        for(var i = 0; i < (lst.length); i++)
         {
-            lst[i].className = "boundry";
+            lst[i].className = "boundary";
         }
     }
-};
+}
 //////////////////////////////////////////////////////////
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("here");
+    /*console.log("here");
     var firstWall = document.getElementById("boundary1");
     console.log(firstWall);
     addFailureClass(firstWall);
@@ -53,6 +64,11 @@ document.addEventListener("DOMContentLoaded", function () {
             break;
         }
     }
-    var x = document.getElementById("end");
-    x.addEventListener("mouseover", function () { completioncheck(); });
+    restartMaze();*/
+    var startingClick = document.getElementById("start");
+    startingClick.addEventListener("click", function () { restartMaze(); });
+    addFailListeners();
+    //console.log("variable data check: " + walls[0]);
+    var endingPosition = document.getElementById("end");
+    endingPosition.addEventListener("mouseover", function () { completioncheck(); });
 });
