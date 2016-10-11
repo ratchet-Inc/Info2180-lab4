0380ed45c0ae87b2669f2484382ce7f0acfc937e
ratchet-Inc <deniroturboman@hotmail.com>
2016-10-11 10:08:37 -0700

    new Completion.

--- maze.js
+++ maze.js
@@ -1,13 +1,50 @@
 // JavaScript source code
 
+var startValue = 0;
+
 function addWallListneers(obj) {
     console.log(obj.length);
     for (var i = 0; i < obj.length - 1; i++) {
         console.log(i);
         obj[i].addEventListener("mouseover", function () {
-            this.className += " youlose";
+            if (startValue == 1) {
+                this.className += " youlose";
+            }
+            completionCheck();
         });
     }
+}
+
+function restartMaze() {
+    console.log("restart Maze sequence");
+    startValue = 1;
+    var lst = document.querySelectorAll(".youlose");
+    if(lst.length > 0)
+    {
+        for (var i = 0; i < lst.length; i++) {
+            lst[i].className = "boundary";
+        }
+    }
+    changeStatus("Now Playing, GOOD LUCK!!");
+}
+
+function completionCheck() {
+    console.log("completion sequence");
+    if(startValue == 1){
+        var lst = document.querySelectorAll(".youlose");
+        if (lst.length > 0) {
+            console.log("failed.");
+            changeStatus("HAHA!!! You LOSE! Better Luck Next Time.");
+        } else {
+            console.log("completed.");
+            changeStatus("HOORAY!!! You Win Good Game.");
+        }
+        startValue = 0;
+    }
+}
+
+function changeStatus(newText) {
+    document.getElementById("status").textContent = newText;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -15,6 +52,9 @@ document.addEventListener("DOMContentLoaded", function () {
     /*document.getElementById("boundary1").addEventListener("mouseover", function () {
         this.className += " youlose";
     });*/
+    changeStatus("CLICK the \'S\' button to start playing.");
     var myLst = document.querySelectorAll(".boundary");
     addWallListneers(myLst);
+    document.getElementById("start").addEventListener("click", function () { restartMaze(); });
+    document.getElementById("end").addEventListener("mouseover", completionCheck);
 });
