4503cc5e5a93ff205fda9928afc05dc669ceed64
ratchet-Inc <deniroturboman@hotmail.com>
2016-10-10 22:56:39 -0700

    improvements

--- lab4/lab4/maze.js
+++ lab4/lab4/maze.js
@@ -1,8 +1,23 @@
 ﻿console.log("test");
 
+var addFailureClass = function (obj) {
+    obj.className += " youlose";
+};
+
 document.addEventListener("DOMContentLoaded", function () {
     console.log("here");
-    var walls = document.getElementById("boundary1");
-    console.log(walls);
-    walls.className += " youlose";
+    var firstWall = document.getElementById("boundary1");
+    console.log(firstWall);
+    addFailureClass(firstWall);
+    var walls = document.querySelectorAll(".boundary");
+    console.log("Query length returned: " + walls.length);
+    for(var i = 1; i < (walls.length) - 1; i++)
+    {
+        addFailureClass(walls[i]);
+        if(i >= walls.length)
+        {
+            console.log("loop error for boundries");
+            break;
+        }
+    }
 });
