6b5d9a76373415a8beadde6012c439413196adda
ratchet-Inc <deniroturboman@hotmail.com>
2016-10-10 23:44:23 -0700

    first listener added

--- lab4/lab4/maze.js
+++ lab4/lab4/maze.js
@@ -37,5 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
             break;
         }
     }
-    completioncheck();
+    var x = document.getElementById("end");
+    x.addEventListener("mouseover", function () { completioncheck(); });
 });
