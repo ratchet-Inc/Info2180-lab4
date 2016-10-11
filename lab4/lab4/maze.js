74b3e4ebc313c5f4b38e6c1c7f2472bcef4aa2b6
ratchet-Inc <deniroturboman@hotmail.com>
2016-10-10 22:19:53 -0700

    progress

---
+++ lab4/lab4/maze.js
@@ -0,0 +1,8 @@
+﻿console.log("test");
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("here");
+    var walls = document.getElementById("boundary1");
+    console.log(walls);
+    walls.className += " youlose";
+});
